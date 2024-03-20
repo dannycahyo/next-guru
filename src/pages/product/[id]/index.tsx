db340ff26a7cbf6707229325e5e18645f727c31a
dannycahyo <dannydwicahyono@gmail.com>
2024-03-20 11:59:19 +0700

    fix: Rename Product component to ProductDetail

--- src/pages/product/[id]/index.tsx
+++ src/pages/product/[id]/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function Product() {
+export default function ProductDetail() {
   const router = useRouter();
   const { id } = router.query;
 
