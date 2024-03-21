4427ec17cf751c24b803f85a681ea22897c2adde
dannycahyo <dannydwicahyono@gmail.com>
2024-03-21 17:06:21 +0700

    feat: adjust folder structuring under src

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,33 @@
-import NextAuth from "next-auth";
+import NextAuth, {
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import prisma from "@/lib/prisma";
-
 import type { NextApiHandler } from "next";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
 
 const authHandler: NextApiHandler = (req, res) =>
   NextAuth(req, res, options);
 export default authHandler;
 
-const options = {
+const options: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
